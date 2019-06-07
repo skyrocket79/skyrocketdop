87578aedf6c2b6fa991894667eaf991ed792a341
Gerardo Miranda <skyrocket.iptv@protonmail.com>
2019-06-06 23:09:09 -0500

    Mi primer Commit

---
+++ Comparacion.py
@@ -0,0 +1,6 @@
+print ("ingresa tue edad:")
+edad= int(input())
+if edad>18:
+    print("Eres mayor de edad")
+else:
+    print("Eres menor de edad")
---
+++ Helloworld.py
@@ -0,0 +1,5 @@
+#Hello World este codigo imprime un mesnaje por pantalla
+print("Ingresa tu nombre")
+saludo="Hola Amigo"
+nombre= input( )
+print(saludo + nombre)
